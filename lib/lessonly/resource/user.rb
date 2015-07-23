6abc49818a99b173f24d7f7cd2846b25dcfb826d
Skylar Anderson <skylar@anderson.is>
2015-07-23 17:22:08 -0400

    adding logic to test if user is assigned to course

--- lib/lessonly/resource/user.rb
+++ lib/lessonly/resource/user.rb
@@ -1,4 +1,7 @@
 module Lessonly
   class User < Resource
+    def assigned_to?(course)
+      course.assignments.map(&:assignee_id).include?(id)
+    end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,17 +1,33 @@
 require 'spec_helper'
 
 describe Lessonly::User do
-  context '#all' do
+  describe '#all' do
     it 'should return all users' do
       users = Lessonly::User.all
       expect(users.count).to eq 3
     end
   end
 
-  context '#find' do
+  describe '#find' do
     it 'should find a single user' do
       user = Lessonly::User.find(1)
       expect(user.name).to eq 'Chas Ballew'
+    end
+  end
+
+  describe '#assigned_to?' do
+    it 'should return true if user is assigned to course' do
+      user = Lessonly::User.find(1)
+      course = Lessonly::Course.find(1)
+
+      expect(user.assigned_to?(course)).to eq true
+    end
+
+    it 'should return false if user is not assigned to course' do
+      user = Lessonly::User.find(3)
+      course = Lessonly::Course.find(1)
+
+      expect(user.assigned_to?(course)).to eq false
     end
   end
 end
