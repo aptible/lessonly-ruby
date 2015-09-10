1da1973ffaf74012baf8f57aef897fe284950ed7
Skylar Anderson <skylar@anderson.is>
2015-09-10 10:16:29 -0400

    serlize resources where necessary

--- lib/lessonly/serializer.rb
+++ lib/lessonly/serializer.rb
@@ -1,13 +1,8 @@
 module Lessonly
   class Serializer < Sawyer::Serializer
     def encode_object(resource)
+      return resource.serialize if resource.respond_to? :serialize
       resource
-      # return resource.serialize if resource.respond_to? :serialize
-      # case resource
-      # when Hash then encode_hash(resource)
-      # when Array then resource.map { |o| encode_object(o) }
-      # else resource
-      # end
     end
 
     def time_field?(key, value)
